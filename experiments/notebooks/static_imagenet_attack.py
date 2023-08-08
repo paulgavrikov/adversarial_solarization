2093047097ca4b528ae530178554b44651d5eccd
Paul Gavrikov <paul.gavrikov@hs-offenburg.de>
2023-08-08 21:40:56 +0200

    adding args to static script

--- experiments/notebooks/static_imagenet_attack.py
+++ experiments/notebooks/static_imagenet_attack.py
@@ -7,6 +7,7 @@ import timm
 import kornia
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+import argparse
 
 
 class NormalizedModel(torch.nn.Module):
@@ -21,49 +22,61 @@ class NormalizedModel(torch.nn.Module):
         out = (x - self.mean) / self.std 
         out = self.model(out)
         return out
-    
 
-dataset = torchvision.datasets.ImageNet("/workspace/data/datasets/imagenet/", 
-                                        split="val", 
-                                        transform=torchvision.transforms.Compose(
-                                            [
-                                                torchvision.transforms.Resize(256), 
-                                                torchvision.transforms.CenterCrop(224), 
-                                                torchvision.transforms.ToTensor()
-                                            ]
-                                        )
-                                       )
 
-dataloader = DataLoader(dataset, batch_size=512, shuffle=True, pin_memory=True, num_workers=16)
+def main(args):
+    dataset = torchvision.datasets.ImageNet(args.imagenet, 
+                                            split="val", 
+                                            transform=torchvision.transforms.Compose(
+                                                [
+                                                    torchvision.transforms.Resize(256), 
+                                                    torchvision.transforms.CenterCrop(224), 
+                                                    torchvision.transforms.ToTensor()
+                                                ]
+                                            )
+                                           )
 
-device = "cuda"
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=16)
 
-model_name = "convnext_base"
+    device = args.device
 
-model = timm.create_model(model_name, pretrained=True)
-model = NormalizedModel(model, model.default_cfg.get("mean"), model.default_cfg.get("std"))
-model = torch.nn.DataParallel(model)
+    model_name = args.model
 
-model.to(device)
-model.eval()
+    model = timm.create_model(model_name, pretrained=True)
+    model = NormalizedModel(model, model.default_cfg.get("mean"), model.default_cfg.get("std"))
+    model = torch.nn.DataParallel(model)
 
-accs = []
+    model.to(device)
+    model.eval()
 
-with torch.no_grad():
-    for alpha in tqdm(np.arange(0, 1.00001, 0.01)):
-        correct = 0
-        total = 0
-        for x, y in (dataloader):
-            x = x.to(device)
-            y = y.to(device)
+    accs = []
 
-            x_aug = kornia.enhance.solarize(x, torch.tensor(alpha).float().to(device))
+    with torch.no_grad():
+        for alpha in tqdm(np.arange(0, 1.00001, 0.01)):
+            correct = 0
+            total = 0
+            for x, y in (dataloader):
+                x = x.to(device)
+                y = y.to(device)
 
-            logits = model(x_aug)
-            correct += (logits.argmax(dim=1) == y).float().sum().item()
-            total += len(y)
+                x_aug = kornia.enhance.solarize(x, torch.tensor(alpha).float().to(device))
 
-        acc = correct / total
-        accs.append(acc)
+                logits = model(x_aug)
+                correct += (logits.argmax(dim=1) == y).float().sum().item()
+                total += len(y)
 
-np.save(f"output/static_imagenet_{model_name}.npy", np.array(accs))
+            acc = correct / total
+            accs.append(acc)
+
+    np.save(f"output/static_imagenet_{model_name}.npy", np.array(accs))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str)
+    parser.add_argument('--device', type=str, default="cuda")
+    parser.add_argument('--batch_size', type=int, default=512)
+    parser.add_argument('--imagenet', type=str, default="/home/SSD/ImageNet/")
+    args = parser.parse_args()
+
+    main(args)
